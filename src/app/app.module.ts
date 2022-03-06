437b76cfccfe6aa1dfd076ef0152b52f630ce933
lokopak <pakpil@gmail.com>
2022-03-06 20:01:04 +0100

    Added suport for window injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,28 @@
-import { NgModule } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { NgModule, PLATFORM_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_WINDOW } from './app.options';
+
+export function windowProviderFactory(platformId: any): Window | undefined {
+  if (isPlatformBrowser(platformId)) {
+    return window;
+  }
+
+  return undefined;
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  providers: [
+    {
+      provide: APP_WINDOW,
+      useFactory: windowProviderFactory,
+      deps: [PLATFORM_ID],
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
