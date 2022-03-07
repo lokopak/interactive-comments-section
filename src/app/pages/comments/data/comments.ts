3a49c17605a0f5d5307cf8a4aeea55873d74eba5
lokopak <pakpil@gmail.com>
2022-03-07 11:57:50 +0100

    Fixed an issue with dates in initial data

--- src/app/pages/comments/data/comments.ts
+++ src/app/pages/comments/data/comments.ts
@@ -1,4 +1,11 @@
+import { formatDate } from '@angular/common';
 import { IComment, IUser } from "../comment";
+import { COMMENT_DATE_FORMAT, COMMENT_LOCALE } from "../comment.config";
+
+const getDateTime = (value: number): string => {
+  let time = Date.now() - value;
+  return formatDate(time, COMMENT_DATE_FORMAT, COMMENT_LOCALE)
+}
 
 export const TempData: {currentUser: IUser, comments: IComment[]} = {
   currentUser: {
@@ -13,7 +20,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
       id: 1,
       content:
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
-      createdAt: '1 month ago',
+      createdAt: getDateTime(1000 * 3600 * 24 * 35),
       score: 12,
       user: {
         image: {
@@ -28,7 +35,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
       id: 2,
       content:
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
-      createdAt: '2 weeks ago',
+      createdAt: getDateTime(1000 * 3600 * 24 * 16),
       score: 5,
       user: {
         image: {
@@ -42,7 +49,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
           id: 3,
           content:
             "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
-          createdAt: '1 week ago',
+          createdAt: getDateTime(1000 * 3600 * 24 * 8),
           score: 4,
           replyingTo: 'maxblagun',
           user: {
@@ -58,7 +65,7 @@ export const TempData: {currentUser: IUser, comments: IComment[]} = {
           id: 4,
           content:
             "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
-          createdAt: '2 days ago',
+          createdAt: getDateTime(1000 * 3600 * 24 * 2.3),
           score: 2,
           replyingTo: 'ramsesmiron',
           user: {
